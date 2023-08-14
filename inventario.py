0774e44c52b69b65558320df9c0e6e13fb819b49
javieer24 <javiermonjesnuevo@gmail.com>
2023-08-14 11:09:40 -0600

    inventario update1.0

--- inventario.py
+++ inventario.py
@@ -1,2 +1,11 @@
 import os
 from tkinter import filedialog
+
+# Función para cargar el inventario inicial desde un archivo .inv
+def cargar_inventario_inicial():
+    # Crea un diccionario vacío para almacenar el inventario
+    inventario = {}
+    
+    # Muestra un cuadro de diálogo para seleccionar el archivo .inv
+    archivo_inv = filedialog.askopenfilename(title="Seleccione el archivo .inv", filetypes=(("Archivos INV", "*.inv"), ("Todos los archivos", "*.*")))
+    
